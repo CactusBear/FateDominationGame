8791e12be0f1ed8d50b665305eab795c8f9b001e
CactusBear <1361075473@qq.com>
2024-05-03 17:09:04 +0800

    完成时点添加

---
+++ editor/Master.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WindowsFormsApp1
+{
+    public class Masters
+    {
+        public Master[] masters;
+    }
+    public class Master
+    {
+        public string master_name { get; set; }
+        public string shown_master_name { get; set; }
+        public string header_img { get; set; }
+        public string master_card_img { get; set; }
+        public string command_spell_img { get; set; }
+        public Effect[] effects { get; set; }
+        public Specials specials { get; set; }
+        public Upgrade_Skill[] upgrade_skill { get; set; }
+    }
+
+    public class Specials
+    {
+        public SKILL[] SKILLS { get; set; }
+        public ATTACK[] ATTACKS { get; set; }
+    }
+
+    public class SKILL
+    {
+        public string skill_name { get; set; }
+        public string skill_card_img { get; set; }
+        public Effect[] effects { get; set; }
+    }
+
+    public class Effect
+    {
+        public string effect_name { get; set; }
+        public string[] time_points { get; set; }
+        public Func[] funcs { get; set; }
+    }
+
+    public class ATTACK
+    {
+        public string attack_name { get; set; }
+        public string attack_crad_img { get; set; }
+        public Effect[] effects { get; set; }
+    }
+
+
+    public class Func
+    {
+        public string func_name { get; set; }
+        public int[] parameters { get; set; }
+    }
+
+    public class Upgrade_Skill
+    {
+        public string skill_name { get; set; }
+        public string skill_card_img { get; set; }
+        public Effect[] effects { get; set; }
+    }
+
+
+
+}
+
---
+++ editor/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Util
+    {
+        public void Write_Json(string str)
+        {
+            //path should be modified
+            File.WriteAllText(Directory.GetCurrentDirectory() + "\\test1.json", str, System.Text.Encoding.UTF8);
+        }
+        public string Read_Json()
+        {
+            //path should be modified
+            string str = File.ReadAllText(Directory.GetCurrentDirectory() + "\\test.json");
+            return str;
+        }
+
+        public class JSON_util
+        {
+            public Masters JSON_to_Master(string jsonstr)
+            {
+                Masters m = Newtonsoft.Json.JsonConvert.DeserializeObject<Masters>(jsonstr);
+                return m;
+            }
+            public string Masters_to_JSON(Masters m)
+            {
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(m);
+                return json;
+            }
+        }
+    }
+}
